604509b8e946da83dfa3dcd949a62d0d4183ef2f
Henry Chen <zchen391@gatech.edu>
2015-09-09 13:35:55 -0400

    finished basic configuration screen

---
+++ src/main/java/ConfigurationScreenController.java
@@ -0,0 +1,114 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
+
+public class ConfigurationScreenController{
+
+    @FXML
+    private Label difficulty_label;
+    @FXML
+    private Label map_label;
+    @FXML
+    private Label players_label;
+    @FXML
+    private Button increase_difficulty;
+    @FXML
+    private Button decrease_difficulty;
+    @FXML
+    private Button increase_map;
+    @FXML
+    private Button decrease_map;
+    @FXML
+    private Button increase_players;
+    @FXML
+    private Button decrease_players;
+
+    private int difficulty = 1;
+    private int map = 1;
+    private int players = 1;
+
+    @FXML
+    private void increaseDifficulty(ActionEvent event) {
+        if (difficulty < 3) {
+            difficulty++;
+        }
+        if (difficulty == 1) {
+            difficulty_label.setText("Beginner");
+        } else if (difficulty == 2) {
+            difficulty_label.setText("Medium");
+        } else if (difficulty == 3) {
+            difficulty_label.setText("Expert");
+        }
+    }
+
+    @FXML
+    private void decreaseDifficulty(ActionEvent event) {
+        if (difficulty > 0) {
+            difficulty--;
+        }
+        if (difficulty == 1) {
+            difficulty_label.setText("Beginner");
+        } else if (difficulty == 2) {
+            difficulty_label.setText("Medium");
+        } else if (difficulty == 3) {
+            difficulty_label.setText("Expert");
+        }
+    }
+
+    @FXML
+     private void increaseMap(ActionEvent event) {
+        if (map < 3) {
+            map++;
+        }
+        if (map == 1) {
+            map_label.setText("Map1");
+        } else if (map == 2) {
+            map_label.setText("Map2");
+        } else if (map == 3) {
+            map_label.setText("Map3");
+        }
+    }
+
+    @FXML
+    private void decreaseMap(ActionEvent event) {
+        if (map > 0) {
+            map--;
+        }
+        if (map == 1) {
+            map_label.setText("Map1");
+        } else if (map == 2) {
+            map_label.setText("Map2");
+        } else if (map == 3) {
+            map_label.setText("Map3");
+        }
+    }
+
+    @FXML
+    private void increasePlayers(ActionEvent event) {
+        if (players < 4) {
+            players++;
+        }
+        players_label.setText(String.valueOf(players));
+    }
+
+    @FXML
+    private void decreasePlayers(ActionEvent event) {
+        if (players > 1) {
+            players--;
+        }
+        players_label.setText(String.valueOf(players));
+    }
+}
+
